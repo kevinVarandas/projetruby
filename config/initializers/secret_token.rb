7fce310d6bb36576f062c024af6fa7d87c82d568
WindcodeUniversity <spiritofcode@gmail.com>
2014-05-27 05:11:10 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Projetruby::Application.config.secret_key_base = '8ad62592ee0beb9e252609665389688512051b38896be23073e6264a8a2601b41b9be98f522feca38e136a0b7329208a9cf52753f061c04e07f4532e9120c72c'
