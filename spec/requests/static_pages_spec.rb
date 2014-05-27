dd349e3938f2b80a76ec53c17b06ed9e3fd1dff0
WindcodeUniversity <spiritofcode@gmail.com>
2014-05-27 07:06:47 -0700

    add a full title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,14 +3,19 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
 
-    it "should have the content 'Home'" do
+    it "should have the content 'Writters App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Home')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Writters App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Writters App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
